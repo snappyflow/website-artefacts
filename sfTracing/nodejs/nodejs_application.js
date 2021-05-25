457c2c2d79ad8d73a451f7c0082576f3315316b1
rajeshbheemanaboyena <53808383+rajeshbheemanaboyena@users.noreply.github.com>
2021-05-25 16:29:49 +0530

    Updated sftrace changes

--- sfTracing/nodejs/nodejs_application.js
+++ sfTracing/nodejs/nodejs_application.js
@@ -1,12 +1,19 @@
 //<SFTRACE-CONFIG> add the below agent specific variable 
 //Replace <service_name> with approariate value. The service_name is used to identify and filter the traces related to an application and should be named appropriately to distinctly identify it.  Service name must only contain characters from the ASCII alphabet, numbers, dashes, underscores and spaces.
-const apm=require('elastic-apm-node').start({
-	  serviceName: '<service_name>',
-	  serverUrl: process.env.SFTRACE_SERVER_URL,
-	  globalLabels: process.env.SFTRACE_GLOBAL_LABELS,
-	  verifyServerCert: process.env.SFTRACE_VERFIY_SERVER_CERT === undefined ? false : process.env.SFTRACE_VERFIY_SERVER_CERT,
-	  active: process.env.SFTRACE_SERVER_URL === undefined ? false : true
-	  })
+try {
+  let sfTraceConfig = JSON.parse(String.fromCharCode.apply(String, require('child_process').execSync("/opt/sfagent/sftrace/sftrace")))
+  const apm = require('elastic-apm-node').start({
+    serviceName: '<service_name>',
+    serverUrl: sfTraceConfig['SFTRACE_SERVER_URL'],
+    globalLabels: sfTraceConfig['SFTRACE_GLOBAL_LABELS'],
+    verifyServerCert: sfTraceConfig['SFTRACE_VERIFY_SERVER_CERT'] === undefined ? false : sfTraceConfig['SFTRACE_VERIFY_SERVER_CERT'],
+    active: sfTraceConfig['SFTRACE_SERVER_URL'] === undefined ? false : true,
+    stackTraceLimit: sfTraceConfig['SFTRACE_STACK_TRACE_LIMIT'],
+    captureSpanStackTraces: sfTraceConfig['SFTRACE_CAPTURE_SPAN_STACK_TRACES']
+  })
+} catch (e) {
+  console.log(e)
+}
 
 
 
