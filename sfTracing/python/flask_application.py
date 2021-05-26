07654cc8feb6fe354726b898d4ece345e2cf9c01
rajeshbheemanaboyena <53808383+rajeshbheemanaboyena@users.noreply.github.com>
2021-05-26 18:51:56 +0530

    Updated sftrace changes

--- sfTracing/python/flask_application.py
+++ sfTracing/python/flask_application.py
@@ -10,13 +10,22 @@ def helloIndex():
 
 #<SFTRACE-CONFIG> add the below agent specific configuration 
 # Replace <service_name> with approariate value. The service_name is used to identify and filter the traces related to an application and should be named appropriately to distinctly identify it.  Service name must only contain characters from the ASCII alphabet, numbers, dashes, underscores and spaces.
-app.config['ELASTIC_APM'] =  {
-	'SERVICE_NAME': '<service-name>',
-	'DEBUG': True,
-	'SERVER_URL': os.getenv( 'SFTRACE_SERVER_URL', None),
-	'GLOBAL_LABELS': os.getenv('SFTRACE_GLOBAL_LABELS', None),
-	'VERIFY_SERVER_CERT': os.getenv('SFTRACE_VERFIY_SERVER_CERT', None),
-}
+import os,json
+try:
+    SFTRACE_CONFIG = json.loads(os.popen('/opt/sfagent/sftrace/sftrace').readlines()[0]) if len(os.popen('/opt/sfagent/sftrace/sftrace').readlines()) > 0 else dict()
+    app.config['ELASTIC_APM'] =  {
+         'SERVICE_NAME': "<service_name>",
+         'SERVER_URL': SFTRACE_CONFIG.get('SFTRACE_SERVER_URL'),
+         'GLOBAL_LABELS': SFTRACE_CONFIG.get('SFTRACE_GLOBAL_LABELS'),
+         'VERIFY_SERVER_CERT': SFTRACE_CONFIG.get('SFTRACE_VERIFY_SERVER_CERT'),
+         'SPAN_FRAMES_MIN_DURATION': SFTRACE_CONFIG.get('SFTRACE_SPAN_FRAMES_MIN_DURATION'),
+         'STACK_TRACE_LIMIT': SFTRACE_CONFIG.get('SFTRACE_STACK_TRACE_LIMIT'),
+         'CENTRAL_CONFIG': False,
+         'DEBUG': True,
+    }
+except Exception as error:
+    print("Error while fetching snappyflow tracing configurations", error)
+
 
 #<SFTRACE-CONFIG> Initialize the agent for this flask application by adding the below line
 apm = ElasticAPM(app)
